4f85986d3c6832f90fc473c0177bed4e63cd2cbf
noma048 <nomad048@gmail.com>
2019-11-13 22:39:32 +0900

    Add validation check

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,12 +43,11 @@ public class SignupController {
 
         //signup.htmlに画面遷移
         return "login/signup";
-
     }
 
     //ユーザー登録画面のPOST用コントローラー
     @PostMapping("/signup")
-    public String postSignUp(@ModelAttribute SignupForm form,BindingResult bindingResult,Model model) {
+    public String postSignUp(@ModelAttribute @Validated SignupForm form,BindingResult bindingResult,Model model) {
         if(bindingResult.hasErrors()) {
             return getSignUp(form, model);
         }
--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -3,19 +3,49 @@ package com.example.demo.login.domain.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
 @Data
 public class SignupForm {
+
+    //必須入力、メールアドレス形式
+    @NotBlank
+    @Email
     private String userId;
+
+    //必須入力、長さ4から100桁までの半角英数のみ
+    @NotBlank
+    @Length(min=4,max=100)
+    @Pattern(regexp="^[a-zA-Z0-9]+$")
     private String password;
+
+    //必須入力
+    @NotBlank
     private String userName;
 
     //ポイント@DateTimeFormat
+    //必須入力
+    @NotNull
     @DateTimeFormat(pattern="yyyy/MM/dd")
     private Date birthday;
+
+    //値が20から100
+    @Min(20)
+    @Max(100)
     private int age;
+
+    //false飲み可能
+    @AssertFalse
     private boolean marriage;
 }
