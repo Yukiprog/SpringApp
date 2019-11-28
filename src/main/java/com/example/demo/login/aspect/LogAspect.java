02e499f925f3dab4b8144f74093b989c23bd4cb4
noma048 <nomad048@gmail.com>
2019-11-29 00:34:29 +0900

    Added AOP execution

---
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -0,0 +1,26 @@
+package com.example.demo.login.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+//ポイント①
+@Aspect
+@Component
+public class LogAspect {
+
+ //ポイント②：AOPの実装
+ @Before("execution(* *..*.*Controller.*(..))")
+ public void stratLog(JoinPoint jp) {
+     System.out.println("メソッド開始: " + jp.getSignature() );
+}
+ //ポイント②：AOPの実装
+ @After("execution(* *..*.*Controller.*(..))")
+ public void endLog(JoinPoint jp) {
+     System.out.println("メソッド終了: " + jp.getSignature() );
+
+ }
+
+}
